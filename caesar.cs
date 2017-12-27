2bca0cde6280d793b6677538750569db3edf91f1
Rodrigo Badin <r.badin@gmail.com>
2017-12-27 21:33:23 -0200

    ceasar 2

--- caesar.cs
+++ caesar.cs
@@ -5,10 +5,12 @@ public class CeasarCypher
     static public void Main()
     {
         string plaintext;
-        // Console.Clear();
+        int numShift;
+        Console.Clear();
         Console.Write("Entre seu texto: ");
         plaintext = Console.ReadLine();
-
+        Console.Write("Mudar por quantas letras? ");
+        numShift = Convert.ToInt32(Console.ReadLine());
         foreach (char c in plaintext)
         {
             if (c == ' ')
@@ -16,15 +18,15 @@ public class CeasarCypher
             else
             {
                 char letter = c;
-                if (letter >= 'X' && letter <= 'Z')
+                if (letter > ('Z' - numShift) && letter <= 'Z')
                 {
                     letter = (char) (letter - 26);
                 }
-                else if (letter >= 'x' && letter <= 'z')
+                else if (letter > ('z' - numShift) && letter <= 'z')
                 {
                     letter = (char) (letter - 26);
                 }
-                Console.Write("{0}", (char) (letter + 3));
+                Console.Write("{0}", (char) (letter + numShift));
             }
         }
         Console.WriteLine("");
